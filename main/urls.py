ebf1282ca31ee0f6b3af05a0f572fea8a8033fed
kamrul <kkamrulhasan2020@gmail.com>
2022-03-23 14:34:37 +0600

    sql model first commit

--- main/urls.py
+++ main/urls.py
@@ -4,5 +4,12 @@ from .import views
 app_name = 'main'
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.HomeView.as_view(), name='home'),
+    path('trade/', views.Trades.as_view(), name='trades'),
+    path('update/', views.update, name='update'),
+    path('delete/', views.delete, name='delete'),
+    path('latest-entry/', views.LatestEntry.as_view(), name='latest_entry'),
+    path('load-data/', views.load_data_from_csv, name='load_data'),
+    path('line-chart/', views.line_chart, name='linechart'),
+    path('bar-chart/', views.bar_chart, name='barchart'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,19 +1,88 @@
-import json
-import os
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from rest_framework import generics
+from django.views.generic import TemplateView
+from .load_data import load
+from .serializers import TradeSerializer
+from .models import Trade, TradeCode
 from django.http import HttpResponse
 from django.http import JsonResponse
 
 
-module_dir = os.path.dirname(__file__)  # get current directory
-file_path = os.path.join(module_dir, 'stock_market_data.json')
+def load_data_from_csv(request):
+    status = load()
+    context = {"status": status}
+    return render(request, "main/load.html", context)
 
-with open(file_path, "r") as read_it:
-     data = json.load(read_it)
+
+class LatestEntry(generics.RetrieveAPIView):
+    serializer_class = TradeSerializer
+
+    def get_object(self):
+        return Trade.objects.latest()
 
 
-def home(request):
-    context = {
-        'data': data,
-    }
-    return render(request,'main/home.html', context, content_type="text/html")
+class Trades(generics.ListCreateAPIView):
+    serializer_class = TradeSerializer
+
+    def get_queryset(self):
+        return Trade.objects.all().order_by('-date')
+
+
+class HomeView(TemplateView):
+    template_name = "main/main-home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["trade_codes"] = TradeCode.objects.all()
+        return context
+
+
+def update(request):
+    pk = request.POST["pk"]
+    attr = request.POST["attr"]
+    val = request.POST["val"]
+    obj = get_object_or_404(Trade, pk=pk)
+    setattr(obj, attr, val)
+    obj.save()
+    return HttpResponse(200)
+
+
+def delete(request):
+    pk = request.POST["pk"]
+    obj = get_object_or_404(Trade, pk=pk)
+    obj.delete()
+    return HttpResponse(200)
+
+
+def line_chart(request):
+    labels = []
+    data = []
+    trade_code = request.POST["trade_code"]
+    trades = Trade.objects.filter(trade_code=trade_code).order_by("date")
+    for entry in trades:
+        labels.append(entry.date)
+        data.append(entry.close)
+    return JsonResponse(
+        data={
+            "labels": labels,
+            "data": data,
+        }
+    )
+
+
+def bar_chart(request):
+    labels = []
+    data = []
+    trade_code = request.POST["trade_code"]
+    trades = Trade.objects.filter(trade_code=trade_code).order_by("date")
+    for entry in trades:
+        labels.append(entry.date)
+        data.append(entry.volume)
+    return JsonResponse(
+        data={
+            "labels": labels,
+            "data": data,
+        }
+    )
+
+
